9b956815deeab931c4f2efe44cd42f45acdaf6fa
Sushant <sushantxt123@gmail.com>
2023-11-17 15:26:44 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about-us-section ptb-100 gray-light-bg" id="why">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 col-lg-6">
+            <div className="about-us-img">
+              <img
+                src="img/about-bg.png"
+                alt="about us"
+                className="img-fluid about-single-img imgimgimg"
+              />
+              <img
+                src="img/about-us-img-bg.svg"
+                alt="about bg"
+                className="about-us-bg"
+              />
+            </div>
+          </div>
+          <div className="col-md-6 col-lg-6">
+            <div className="about-us-content-wrap">
+              <strong className="color-secondary">Viz-A-Thon 2.0 Overview</strong>
+              <h3>What is Viz-A-Thon?</h3>
+              <span className="animate-border mb-4"></span>
+              <p>
+                {" "}
+                After getting the success of{" "}
+                <a href="#">Viz-A-Thon 1.0</a> we are again
+                back with another one Viz-A-Thon 2.0 🔥{" "}
+              </p>
+              <p>
+              Vizathon, a three-hour offline hackathon, tasks participants with cleaning and analyzing provided datasets using tools like Tableau and Power BI. The challenge lies in crafting insightful visualizations from cleaned data. With a prize pool exceeding 2 lakh rupees, participants clean, analyze, and present their findings in visually compelling dashboards to a panel of judges. Judging criteria encompass data preparation, analysis depth, visualization design, presentation, and tool proficiency. This event not only fosters skill development in data handling but also offers networking, recognition, and career advancement opportunities. Vizathon is the ultimate platform where data prowess meets creativity in a high-stakes, high-reward competition.
+              </p>
+              <div className="row mt-4">
+                <div className="col-4">
+                  <div className="counter-single">
+                    <h2 className="mb-0 color-secondary">6</h2>
+                    <strong>Themes </strong>
+                  </div>
+                </div>
+                <div className="col-4">
+                  <div className="counter-single">
+                    <h2 className="mb-0 color-secondary">5+</h2>
+                    <strong>
+                      {" "}
+                      Hours of Visualization <br /> Including Expert Sessions & Talks
+                    </strong>
+                  </div>
+                </div>
+                <div className="col-4">
+                  <div className="counter-single">
+                    <h2 className="mb-0 color-secondary">200+</h2>
+                    <strong>Total Particpants</strong>
+                  </div>
+                </div>
+              </div>
+              <div className="action-btns mt-4">
+                <a
+                  href="https://discord.gg/cvHPEK7U3v"
+                  className="btn secondary-outline-btn"
+                  target="_blank"
+                >
+                  Join Discord Channel
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/Schedule.jsx
@@ -0,0 +1,166 @@
+import React from "react";
+
+const Schedule = () => {
+  return (
+    <section
+      className="work-process-section ptb-100 gray-light-bg"
+      id="schedule"
+    >
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-10 col-lg-9">
+            <div className="section-heading text-center">
+              <strong className="color-secondary">When to Join?</strong>
+              <h2> Viz-A-Thon Schedule </h2>
+              <span className="animate-border mr-auto ml-auto mb-4"></span>
+            </div>
+          </div>
+        </div>
+        <div className="flexxx">
+          <div className="row">
+            <div className="col-md-12 col-lg-5 process-width">
+              <div className="process-box process-left">
+                <div className="row">
+                  <div className="col-md-5">
+                    <div className="process-step-number">
+                      <strong>20th Nov</strong>
+                      <h6>10:OO AM</h6>
+                    </div>
+                  </div>
+                  <div className="col-md-7">
+                    <h5>Registration Starts</h5>
+                    <p>
+                      Register yourself at VIERP by submitting your details.{" "}
+                    </p>
+                  </div>
+                </div>
+                <div className="process-line-r"></div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12 col-lg-5 process-width">
+              <div className="process-box process-left">
+                <div className="row">
+                  <div className="col-md-5">
+                    <div className="process-step-number">
+                      <strong>10th Jan</strong>
+                      <h6>10:OO PM</h6>
+                    </div>
+                  </div>
+                  <div className="col-md-7">
+                    <h5>Registration Ends</h5>
+                    <p>We'll review your appication and will let you know. </p>
+                  </div>
+                </div>
+                <div className="process-line-r"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="flexxx">
+        <div className="row">
+            <div className="col-md-12 col-lg-5 process-width">
+              <div className="process-box process-left">
+                <div className="row">
+                  <div className="col-md-5">
+                    <div className="process-step-number">
+                      <strong>16th Jan </strong>
+                      <h6> 12:00 PM</h6>
+                    </div>
+                  </div>
+                  <div className="col-md-7">
+                    <h5>Viz-A-Thon Starts 🥳</h5>
+                    <p>
+                      You can start working on datasets after
+                      inauguration ceremony. <br />{" "}
+                    </p>
+                  </div>
+                </div>
+                <div className="process-line-l"></div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12 col-lg-5 process-width">
+              <div className="process-box process-left">
+                <div className="row">
+                  <div className="col-md-5">
+                    <div className="process-step-number">
+                      <strong>16th Jan</strong>
+                      <h6>03:3O PM</h6>
+                    </div>
+                  </div>
+                  <div className="col-md-7">
+                    <h5>Submit Final Work</h5>
+                    <p>Team leader should submit the insights on Devfolio.&emsp;&emsp;</p>
+                  </div>
+                </div>
+                <div className="process-line-r"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flexxx">
+          <div className="row">
+            <div className="col-md-12 col-lg-5 process-width">
+              <div className="process-box process-left">
+                <div className="row">
+                  <div className="col-md-5">
+                    <div className="process-step-number">
+                      <strong>16th Jan </strong>
+                      <h6> 04:00 PM</h6>
+                    </div>
+                  </div>
+                  <div className="col-md-7">
+                    <h5>Present The Visuals</h5>
+                    <p>
+                      Our judges will go through your work and give you points.<br />{" "}
+                    </p>
+                  </div>
+                </div>
+                <div className="process-line-l"></div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12 col-lg-5 process-width">
+              <div className="process-box process-left">
+                <div className="row">
+                  <div className="col-md-5">
+                    <div className="process-step-number">
+                      <strong>16th Jan</strong>
+                      <h6>06:00 PM</h6>
+                    </div>
+                  </div>
+                  <div className="col-md-7">
+                    <h5>Hackathon Ends</h5>
+                    <p>
+                      We'll declare the winners and Viz-A-Thon ends!{" "}
+                      {/* <a
+                        style={{ color: "blue" }}
+                        target={"_blank"}
+                        className="linkk"
+                        href="https://duhacks2.devfolio.co"
+                      >
+                        duhacks
+                      </a>{" "}
+                      devfolio dashboard.&emsp;&emsp;&emsp; &emsp;
+                      {""}{" "} */}
+                    </p>
+                  </div>
+                </div>
+                <div className="process-line-r"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Schedule;
